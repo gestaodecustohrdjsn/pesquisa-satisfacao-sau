b36c773461e00fb08ed30c345f30c9d31a208d4b
gestaodecustohrdjsn <gestaodecustohrdjsn@gmail.com>
2025-12-29 09:58:05 -0400

    Update app.js
    Atualização 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-teste
+const URL_SCRIPT = "COLE_AQUI_SUA_URL_DO_APPS_SCRIPT";
+
+function responder(resposta) {
+
+  fetch(URL_SCRIPT, {
+    method: "POST",
+    body: JSON.stringify({
+      atendimento: resposta
+    })
+  });
+
+  trocarTela("telaFinal");
+
+  setTimeout(() => {
+    location.reload();
+  }, 4000);
+}
+
+function trocarTela(id) {
+  document.querySelectorAll(".tela").forEach(t => t.classList.remove("ativa"));
+  document.getElementById(id).classList.add("ativa");
+}
